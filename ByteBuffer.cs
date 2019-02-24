b8316e40dcf7ae773e2ff5bce2c442478745d00a
zillakot <markus.korpinen@gmail.com>
2019-02-24 23:45:39 +0200

    added read bytebuffer methods and idisposable interface

--- ByteBuffer.cs
+++ ByteBuffer.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GameServer 
 {
-    public class ByteBuffer
+    public class ByteBuffer : IDisposable
     {
         private List<byte> Buff;
         private byte[] readBuff;
@@ -72,6 +72,201 @@ namespace GameServer
             Buff.AddRange(BitConverter.GetBytes(input.Length));
             Buff.AddRange(Encoding.UTF8.GetBytes(input));
             buffUpdated = true;
+        }
+        public byte ReadByte(bool Peek = true)
+        {
+            if(Buff.Count > readPos){
+                if(buffUpdated)
+                {
+                    readBuff = Buff.ToArray();
+                    buffUpdated = false;
+                }
+
+                byte value = readBuff[readPos];
+                if(Peek & Buff.Count > readPos)
+                {
+                    readPos += 1;
+                }
+
+                return value;
+            }
+            else
+            {
+                throw new Exception("Type is not a byte");
+            }
+        }
+
+        public byte[] ReadBytes(int Length, bool Peek = true)
+        {
+            if(buff.Count > readPos){
+                if(buffUpdated)
+                {
+                    readBuff = Buff.ToArray();
+                    buffUpdated = false;
+                }
+
+                byte[] value = Buff.GetRange(readPos, Length).ToArray();
+                if(Peek)
+                {
+                    readPos += Length;
+                }
+
+                return value;
+            }
+            else
+            {
+                throw new Exception("Type is not a byte array");
+            }
+        }
+        public short ReadShort(bool Peek = true)
+        {
+            if(buff.Count > readPos){
+                if(buffUpdated)
+                {
+                    readBuff = Buff.ToArray();
+                    buffUpdated = false;
+                }
+
+                short value = BitConverter.ToInt16(readBuff, readPos);
+                if(Peek  & Buff.Count > readPos)
+                {
+                    readPos += 2;
+                }
+
+                return value;
+            }
+            else
+            {
+                throw new Exception("Type is not a short");
+            }
+        }
+        public int ReadInteger(bool Peek = true)
+        {
+            if(buff.Count > readPos){
+                if(buffUpdated)
+                {
+                    readBuff = Buff.ToArray();
+                    buffUpdated = false;
+                }
+
+                int value = BitConverter.ToInt32(readBuff, readPos);
+                if(Peek  & Buff.Count > readPos)
+                {
+                    readPos += 4;
+                }
+
+                return value;
+            }
+            else
+            {
+                throw new Exception("Type is not a int");
+            }
+        }
+        public long ReadLong(bool Peek = true)
+        {
+            if(buff.Count > readPos){
+                if(buffUpdated)
+                {
+                    readBuff = Buff.ToArray();
+                    buffUpdated = false;
+                }
+
+                long value = BitConverter.ToInt64(readBuff, readPos);
+                if(Peek  & Buff.Count > readPos)
+                {
+                    readPos += 8;
+                }
+
+                return value;
+            }
+            else
+            {
+                throw new Exception("Type is not a long");
+            }
+        }
+        public float ReadFloat(bool Peek = true)
+        {
+            if(buff.Count > readPos){
+                if(buffUpdated)
+                {
+                    readBuff = Buff.ToArray();
+                    buffUpdated = false;
+                }
+
+                float value = BitConverter.ToSingle(readBuff, readPos);
+                if(Peek  & Buff.Count > readPos)
+                {
+                    readPos += 4;
+                }
+
+                return value;
+            }
+            else
+            {
+                throw new Exception("Type is not a single");
+            }
+        }
+        public bool ReadBool(bool Peek = true)
+        {
+            if(buff.Count > readPos){
+                if(buffUpdated)
+                {
+                    readBuff = Buff.ToArray();
+                    buffUpdated = false;
+                }
+
+                bool value = BitConverter.ToBoolean(readBuff, readPos);
+                if(Peek  & Buff.Count > readPos)
+                {
+                    readPos += 1;
+                }
+
+                return value;
+            }
+            else
+            {
+                throw new Exception("Type is not a bool");
+            }
+        }
+        public string ReadString(bool Peek = true)
+        {
+            try{
+                int length = ReadInteger(true);
+                if(buffUpdated)
+                {
+                    readBuff = Buff.ToArray();
+                    buffUpdated = false;
+                }
+
+                string value = Encoding.UTF8.GetString(readBuff, readPos, length);
+                if(Peek  & Buff.Count > readPos)
+                {
+                    if(value.Length > 0)
+                        readPos += length;
+                }
+                
+                return value;
+            }
+            catch
+            {
+                throw new Exception("Type is not a string");
+            }
+        }
+
+        private bool disposedValue = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposedValue){
+                if(disposing){
+                    Buff.Clear();
+                    readPos = 0;
+                }
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
         }
     }
 }
